3a8687c52abbdb0ef048e56394df2f095794186d
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-01-05 07:17:25 +0000

    DOM & custom arrow function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+const button = document.getElementById('button');
+const showNumnberDiv = document.getElementById('show-generated-number');
 
+const generateRandomNumber = () => {
+	const randomNumber = Math.ceil(Math.random() * 28); // 1 to 28 (inclusive)
+	showNumnberDiv.innerHTML = randomNumber;
+};
+
+button.addEventListener('click', generateRandomNumber);
